7fd12c16ad3a2f2ee64b7b89b911f96660bd5e02
Thomas Teoh <me@tteoh.com>
2016-01-31 10:00:33 +1100

    First commit, uploading the run_analysis.R files

---
+++ run_analysis.R
@@ -0,0 +1,40 @@
+setwd("/media/sf_Dropbox/Technology/R/coursera/3 Getting and Cleaning Data/assignment/")
+
+library(dplyr)
+
+# Features
+features <- read.table(unz("getdata-projectfiles-UCI HAR Dataset.zip", "UCI HAR Dataset/features.txt"))
+
+# Load training and testing data
+training <- read.csv(unz("getdata-projectfiles-UCI HAR Dataset.zip", "UCI HAR Dataset/train/X_train.txt"), sep="", header=FALSE)
+training.act <- read.csv(unz("getdata-projectfiles-UCI HAR Dataset.zip", "UCI HAR Dataset/train/y_train.txt"), header=FALSE)
+training.sub <- read.csv(unz("getdata-projectfiles-UCI HAR Dataset.zip", "UCI HAR Dataset/train/subject_train.txt"), header=FALSE)
+
+testing <- read.csv(unz("getdata-projectfiles-UCI HAR Dataset.zip", "UCI HAR Dataset/test/X_test.txt"), sep="", header=FALSE)
+testing.act <- read.csv(unz("getdata-projectfiles-UCI HAR Dataset.zip", "UCI HAR Dataset/test/y_test.txt"), header=FALSE)
+testing.sub <- read.csv(unz("getdata-projectfiles-UCI HAR Dataset.zip", "UCI HAR Dataset/test/subject_test.txt"), header=FALSE)
+
+# Putting them into data frames and combining them
+training.1 <- data.frame(training.sub, training.act, training)
+testing.1 <- data.frame(testing.sub, testing.act, testing)
+
+activity.data <- rbind(training.1, testing.1)
+
+# Add column names and convert them to valid characters for dplyr
+colnames(activity.data) <- c("subject.id", "activity.labels", as.vector(features[, 2]))
+names(activity.data) <- make.names(names=names(activity.data), unique=TRUE, allow_ = TRUE)
+
+# Selecting which columns have relevant information (subject, label, mean, std) and don't contain other information (freq, angle)
+activity.data <- select(activity.data, contains("subject"), contains("label"), contains("mean"), contains("std"), -contains("freq"), -contains("angle"))
+
+# Activity label names
+labels <- read.table(unz("getdata-projectfiles-UCI HAR Dataset.zip", "UCI HAR Dataset/activity_labels.txt"))
+
+# Labelling
+activity.data$activity.labels <- as.character(labels[match(activity.data$activity.labels, labels$V1), 'V2'])
+
+# Calculate averages and write to a new file
+averages <- aggregate(. ~subject.id + activity.labels, activity.data, mean)
+averages <- averages[order(averages$subject.id),]
+
+write.table(averages, file = "run_analysis_averages.txt", row.names = FALSE)
